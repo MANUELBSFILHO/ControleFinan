f28933c843ae78437ea37d1c8a0f2a98186bb49f
MANUELBSFILHO <mbsfilho.engenharia@gmail.com>
2023-12-04 09:03:28 -0300

    função exportar e logo

--- script.js
+++ script.js
@@ -92,4 +92,45 @@ const getItensBD = () => JSON.parse(localStorage.getItem("db_items")) ?? [];
 const setItensBD = () =>
   localStorage.setItem("db_items", JSON.stringify(items));
 
+// Função para imprimir o relatório
+function printReport() {
+  window.print();
+}
+
+// Função para exportar o relatório como arquivo de texto
+function exportReport() {
+  const totalIncomes = parseFloat(incomes.textContent);
+  const totalExpenses = parseFloat(expenses.textContent);
+  const balance = totalIncomes - totalExpenses;
+
+  let reportText = `Relatório de Despesas e Receitas\n\n`;
+  
+  reportText += `Total de Receitas: R$ ${totalIncomes.toFixed(2)}\n`;
+  reportText += `Total de Despesas: R$ ${totalExpenses.toFixed(2)}\n`;
+  reportText += `Saldo: R$ ${balance.toFixed(2)}\n\n`;
+
+  if (items.length > 0) {
+    reportText += `Detalhes:\n`;
+
+    items.forEach((item, index) => {
+      reportText += `${index + 1}. ${item.desc} - R$ ${item.amount} (${item.type})\n`;
+    });
+  } else {
+    reportText += `Nenhuma transação registrada.`;
+  }
+
+  const blob = new Blob([reportText], { type: 'text/plain' });
+  const url = URL.createObjectURL(blob);
+
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = 'relatorio.txt';
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+
+  // Limpa a URL do objeto Blob
+  URL.revokeObjectURL(url);
+}
+
 loadItens();
